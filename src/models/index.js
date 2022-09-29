b71bdb5433f41b7a5c7136264e1f76e65cb5b5a3
Robert Shepley <shepleysound@gmail.com>
2022-09-28 20:49:07 -0700

    Get API v1 functionality working.

--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,22 @@
 'use strict';
 
-const userModel = require('./users.js');
 const { Sequelize, DataTypes } = require('sequelize');
+const userModel = require('./users/model.js');
+const foodModel = require('./food/model');
+const bookModel = require('./books/model');
+const Collection = require('./data-collection');
 
 const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory';
 
 const sequelize = new Sequelize(DATABASE_URL);
 
+const users = userModel(sequelize, DataTypes);
+const food = foodModel(sequelize, DataTypes);
+const books = bookModel(sequelize, DataTypes);
+
 module.exports = {
   db: sequelize,
-  users: userModel(sequelize, DataTypes),
+  users: users,
+  food: new Collection(food),
+  books: new Collection(books),
 };
--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ const notFound = require('./error-handlers/404');
 const errorHandler = require('./error-handlers/500');
 const logger = require('./middleware/logger.js');
 const authRouter = require('./auth/routes');
+const dataRouter = require('./routes/v1');
 
 // Require routes here?
 console.log('MAKE SURE YOU REQUIRE YOUR ROUTES');
@@ -23,7 +24,8 @@ app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(authRouter);
+app.use('/auth/', authRouter);
+app.use('/api/v1', dataRouter);
 
 // Catch-all route (404)
 app.use('*', notFound);
